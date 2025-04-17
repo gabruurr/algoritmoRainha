96cdf7833a432f5ad859bd047a1873a7c0756aa6
unknown <gabryelaraujo2019@gmail.com>
2025-04-17 00:07:49 -0300

    feat: adicionada função para desenhar a matriz

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 MOV_CAVALO = [(-2, -1), (-1, -2), (1, -2), (2, -1),
               (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+def gerar_tabuleiro(n, posicoes_rainhas):
+    """vai desenhar uma matriz com as rainhas posicionadas."""
+    tabuleiro = [['.' for _ in range(n)] for _ in range(n)]
+    for linha in range(n):
+        col = posicoes_rainhas[linha]
+        tabuleiro[linha][col] = 'R'  
+    return tabuleiro
